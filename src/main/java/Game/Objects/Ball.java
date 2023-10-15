3eaa4f1eb31297946903a029c6357bc1b0d5a942
vitor0p9f <dev.vitorpfontes002@gmail.com>
2023-10-14 21:24:31 -0300

    Mudando a logica de reset ao realizar um ponto
    Resetando apenas a posição da bola quando um ponto é feito dentro do game.

--- src/main/java/Game/Objects/Ball.java
+++ src/main/java/Game/Objects/Ball.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Game.Objects;
-import java.awt.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
 import java.util.Random;
 
 /**
@@ -11,22 +13,23 @@ import java.util.Random;
  * @author VPF
  */
 public class Ball extends GameObject {
-    public int xDirection, yDirection, WINDOW_WIDTH,WINDOW_HEIGHT;
+    public int xDirection, yDirection, WINDOW_WIDTH, WINDOW_HEIGHT;
     // Definindo a classe GameObject como superclasse da classe Ball
     // ball constructor assigns values to instance variables
-    
+
     /**
-     * @param xPosition - Posição da bola no eixo x
-     * @param yPosition - Posição da bola no eixo y
-     * @param xDirection - Direção da bola no eixo x
-     * @param yDirection - Direção da bola no eixo y
-     * @param speed - Velocidade da bola
-     * @param color - Cor da bola
-     * @param size - Tamanho do diâmetro da bola
-     * @param WINDOW_WIDTH - LArgura da tela onde a bola está sendo renderizada
+     * @param xPosition     - Posição da bola no eixo x
+     * @param yPosition     - Posição da bola no eixo y
+     * @param xDirection    - Direção da bola no eixo x
+     * @param yDirection    - Direção da bola no eixo y
+     * @param speed         - Velocidade da bola
+     * @param color         - Cor da bola
+     * @param size          - Tamanho do diâmetro da bola
+     * @param WINDOW_WIDTH  - LArgura da tela onde a bola está sendo renderizada
      * @param WINDOW_HEIGHT
      */
-    public Ball(int xPosition, int yPosition,int xDirection, int yDirection, int speed, Color color, int size, int WINDOW_WIDTH,int WINDOW_HEIGHT) {
+    public Ball(int xPosition, int yPosition, int xDirection, int yDirection, int speed, Color color, int size,
+            int WINDOW_WIDTH, int WINDOW_HEIGHT) {
         super(xPosition, yPosition, size, size, speed, color);
         this.xDirection = xDirection;
         this.yDirection = yDirection;
@@ -38,17 +41,18 @@ public class Ball extends GameObject {
     public void paint(Graphics g) {
         g.setColor(color); // define a cor do pincel para a cor da bola
 
-        g.fillOval(xPosition, yPosition,this.width, this.width);// desenha a bola na posição x,y com largura e altura de size
+        g.fillOval(xPosition, yPosition, this.width, this.width);// desenha a bola na posição x,y com largura e altura
+                                                                 // de size
     }
 
     // Faz a bola avançar uma determinada quantidade de unidades numa determinada
     // direção
     public void moveBall() {
-        this.xPosition += (this.speed*this.xDirection);
-        this.yPosition += (this.speed*this.yDirection);
+        this.xPosition += (this.speed * this.xDirection);
+        this.yPosition += (this.speed * this.yDirection);
     }
-    
-//Isso é lógica do jogo, não da bola
+
+    // Isso é lógica do jogo, não da bola
     /**
      * Faz a bola rebater nas bordas inferiores e superiores da tela
      * 
@@ -71,30 +75,29 @@ public class Ball extends GameObject {
         this.xDirection *= -1;
     }
 
-    //Verifica se a bola saiu da tela
+    // Verifica se a bola saiu da tela
     public boolean exitOfScreen() {
-        if(this.getXPosition() < 0 || this.getXPosition() > this.WINDOW_WIDTH){
+        if (this.getXPosition() < 0 || this.getXPosition() > this.WINDOW_WIDTH) {
             return true;
-        }
-        else return false;
+        } else
+            return false;
     }
-    
-    //Caso a bola tenha saído, coloca ela novamente no centro da tela e aleatoriza a direção que ela vai movimentar
-    public void respawnBall(){
 
-        this.xPosition = this.WINDOW_WIDTH/2;
-        this.yPosition = this.WINDOW_HEIGHT/2;
-            
-        //Aleatoriza  a direção que a bola seguirá
-            
+    // Caso a bola tenha saído, coloca ela novamente no centro da tela e aleatoriza
+    // a direção que ela vai movimentar
+    public void respawn() {
+        this.xPosition = this.WINDOW_WIDTH / 2;
+        this.yPosition = this.WINDOW_HEIGHT / 2;
+
+        // Aleatoriza a direção que a bola seguirá
+
         // Gera uma matriz pra armazenar todas as opções possíveis de direção
-        int options[][] = {{1,1},{1,-1},{-1,1},{-1,-1}};
-            
-        //Gera o valor true ou false aleatoreamente e armazena numa variável
-        boolean randomBooleanValue = new Random().nextBoolean();
-            
-        this.yDirection = randomBooleanValue ? 1 : -1;
-        this.xDirection = randomBooleanValue ? -1: 1;
+        int respawnOptions[][] = { { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } };
 
+        // Gera o valor true ou false aleatoreamente e armazena numa variável
+        boolean randomBooleanValue = new Random().nextBoolean();
+
+        this.yDirection = randomBooleanValue ? 1 : -1;
+        this.xDirection = randomBooleanValue ? -1 : 1;
     }
 }
--- src/main/java/Game/Screens/GameScreen.java
+++ src/main/java/Game/Screens/GameScreen.java
@@ -4,16 +4,16 @@
  */
 package Game.Screens;
 
-import Game.Controllers.PlayerController;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 
-import Game.Screens.Elements.Score.Score;
+import Game.Controllers.PlayerController;
 import Game.Objects.Ball;
 import Game.Objects.Paddle;
+import Game.Screens.Elements.Score.Score;
 import System.Utilities.MyKeyListener;
 
 /**
@@ -29,11 +29,14 @@ public class GameScreen extends JPanel {
 
     public GameScreen(MyKeyListener keyListener) {
         // Instancia os objetos do jogo
-        this.gameBall = new Ball(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 1, 1, 3, Color.YELLOW, 10,WINDOW_WIDTH,WINDOW_HEIGHT);
+        this.gameBall = new Ball(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 1, 1, 3, Color.YELLOW, 10, WINDOW_WIDTH,
+                WINDOW_HEIGHT);
         this.gamePaddle1 = new Paddle(0, 50, 150, 5, Color.yellow);
         this.gamePaddle2 = new Paddle((WINDOW_WIDTH - 15), 150, 150, 5, Color.green);
-        this.player1Controller = new PlayerController(KeyEvent.VK_W,KeyEvent.VK_S,gamePaddle1,keyListener,WINDOW_HEIGHT);
-        this.player2Controller = new PlayerController(KeyEvent.VK_UP,KeyEvent.VK_DOWN,gamePaddle2,keyListener,WINDOW_HEIGHT);
+        this.player1Controller = new PlayerController(KeyEvent.VK_W, KeyEvent.VK_S, gamePaddle1, keyListener,
+                WINDOW_HEIGHT);
+        this.player2Controller = new PlayerController(KeyEvent.VK_UP, KeyEvent.VK_DOWN, gamePaddle2, keyListener,
+                WINDOW_HEIGHT);
         this.player1 = new Score(0);
         this.player2 = new Score(0);
     }
@@ -53,7 +56,8 @@ public class GameScreen extends JPanel {
         gamePaddle2.paint(g);
 
         g.setColor(Color.WHITE);
-        g.drawString("SCORE - PLAYER 1 [" + player1.getPlayerScore() + "]  PLAYER 2 ["+ player2.getPlayerScore() + "]", 250, 20);
+        g.drawString("SCORE - PLAYER 1 [" + player1.getPlayerScore() + "]  PLAYER 2 [" + player2.getPlayerScore() + "]",
+                250, 20);
     }
 
     /**
@@ -62,7 +66,7 @@ public class GameScreen extends JPanel {
     public void gameLogic() {
         // Movimenta a bola
         gameBall.moveBall();
-        
+
         // Verifica se há colisão da bola com a borda inferior ou superior e aplica a
         // lógica
         gameBall.bounceOnEdges(0, WINDOW_HEIGHT);
@@ -72,33 +76,24 @@ public class GameScreen extends JPanel {
             gameBall.reverseXDirection();
         }
 
-        //Verifica se há comandos de movimentação para os paddles
+        // Verifica se há comandos de movimentação para os paddles
         player1Controller.checkForMoveUpCommand();
         player1Controller.checkForMoveDownCommand();
         player2Controller.checkForMoveUpCommand();
         player2Controller.checkForMoveDownCommand();
 
-        //Verifica se a bola saiu da tela, ou seja se algum jogador pontuou,
-        // e então incrementa a pontuação do jogador e ajusta as posições dos objetos do jogo
+        /*
+         * Verifica se a bola saiu da tela, ou seja se algum jogador pontuou,
+         * e então incrementa a pontuação do jogador e redefine a posição da bola
+         */
         if (gameBall.exitOfScreen()) {
             if (gameBall.getXPosition() > 10) {
                 player1.newScore();
-                reset();
-            }
-            else {
+            } else {
                 player2.newScore();
-                reset();
             }
+
+            gameBall.respawn();
         }
-
-    }
-
-    //Recomeça o jogo com os objetos movidos para as posições iniciais do jogo
-    public void reset() {
-        gameBall.respawnBall();
-        gamePaddle1.setXPosition(0);
-        gamePaddle1.setYPosition(50);
-        gamePaddle2.setXPosition(WINDOW_WIDTH - 15);
-        gamePaddle2.setYPosition(150);
     }
 }
